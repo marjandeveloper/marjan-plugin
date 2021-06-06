9db62be1b69495c13a74239f493d0c0a26ad3376
anjaaleksa <marjan1505@gmail.com>
2021-06-06 13:26:10 +0200

    add header to my plugin

---
+++ marjan-plugin.php
@@ -0,0 +1,14 @@
+<?php 
+
+/* 
+* @package MarjanPlugin
+*/
+
+/* 
+Plugin Name: Marjan Plugin
+Plugin URI: https://github.com/marjandeveloper/marjan-plugin
+Description: This is my plugin for presentation
+Version: 1.0.0
+Author: Marjan Nikolic
+Author URI: https://marjan1505.wordpress.com/
+*/
