0469e1e54134db548cf212a407e038a0bfaf211c
anjaaleksa <marjan1505@gmail.com>
2021-06-06 13:47:05 +0200

    Add index.php

--- marjan-plugin.php
+++ marjan-plugin.php
@@ -11,6 +11,24 @@ Description: This is my plugin for presentation
 Version: 1.0.0
 Author: Marjan Nikolic
 Author URI: https://marjan1505.wordpress.com/
-Licence: GPLv2 or later
+Licence: GNU General Public License v2 or later
 Text Domain: marjan-plugin
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
 */
