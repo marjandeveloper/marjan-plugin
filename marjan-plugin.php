f296840e005d0fa2e1051b9657d8209a3d911d00
anjaaleksa <marjan1505@gmail.com>
2021-06-06 13:29:01 +0200

    add header

--- marjan-plugin.php
+++ marjan-plugin.php
@@ -11,4 +11,6 @@ Description: This is my plugin for presentation
 Version: 1.0.0
 Author: Marjan Nikolic
 Author URI: https://marjan1505.wordpress.com/
+Licence: GPLv2 or later
+Text Domain: marjan-plugin
 */
